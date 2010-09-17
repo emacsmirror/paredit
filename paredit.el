db3a581bf6499249f40bdecc9fe0dc94fe74b9f9
Taylor R Campbell <campbell@mumble.net>
2010-09-17 19:34:00 +0000

    Switch copying terms to GPLv3+.
    Ignore-this: 48fa16729dac3b8da693f85aaea5ccf0
    
    darcs-hash:20100917193400-00fcc-c4c7e044c3906369cc383e3b63fdc6412c332067

--- paredit.el
+++ paredit.el
@@ -7,35 +7,20 @@
 ;;; THIS FILE IS SUBJECT TO CHANGE, AND NOT SUITABLE FOR DISTRIBUTION
 ;;; BY PACKAGE MANAGERS SUCH AS APT, PKGSRC, MACPORTS, &C.
 
-;;; Copyright (c) 2008, 2009, Taylor R. Campbell
+;;; Copyright (c) 2005--2010, Taylor R. Campbell
 ;;;
-;;; Redistribution and use in source and binary forms, with or without
-;;; modification, are permitted provided that the following conditions
-;;; are met:
+;;; Paredit is free software: you can redistribute it and/or modify it
+;;; under the terms of the GNU General Public License as published by
+;;; the Free Software Foundation, either version 3 of the License, or
+;;; (at your option) any later version.
 ;;;
-;;; * Redistributions of source code must retain the above copyright
-;;;   notice, this list of conditions and the following disclaimer.
+;;; Paredit is distributed in the hope that it will be useful, but
+;;; WITHOUT ANY WARRANTY; without even the implied warranty of
+;;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+;;; General Public License for more details.
 ;;;
-;;; * Redistributions in binary form must reproduce the above copyright
-;;;   notice, this list of conditions and the following disclaimer in
-;;;   the documentation and/or other materials provided with the
-;;;   distribution.
-;;;
-;;; * Neither the names of the authors nor the names of contributors
-;;;   may be used to endorse or promote products derived from this
-;;;   software without specific prior written permission.
-;;;
-;;; THIS SOFTWARE IS PROVIDED BY THE AUTHORS ``AS IS'' AND ANY EXPRESS
-;;; OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
-;;; WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
-;;; ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHORS BE LIABLE FOR ANY
-;;; DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
-;;; DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE
-;;; GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
-;;; INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
-;;; WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
-;;; NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
-;;; SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+;;; You should have received a copy of the GNU General Public License
+;;; along with paredit.  If not, see <http://www.gnu.org/licenses/>.
 
 ;;; This file is permanently stored at
 ;;;   <http://mumble.net/~campbell/emacs/paredit-22.el>.
