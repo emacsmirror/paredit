42a0494e860a612c1ae18a39504faf456dd787e6
Taylor R Campbell <campbell@mumble.net>
2013-06-13 20:00:52 +0000

    Use $EMACS in genhtml.sh too.

--- genhtml.sh
+++ genhtml.sh
@@ -2,7 +2,9 @@
 
 set -Ceu
 
-exec emacs --batch --load paredit.el --eval '
+: ${EMACS:=emacs}
+
+exec ${EMACS} --batch --load paredit.el --eval '
 (with-temp-buffer
   (paredit-insert-html-examples)
   (write-file "paredit.html"))
