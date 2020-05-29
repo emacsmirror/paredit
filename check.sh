92aaa712be6185a105638c551f46befaf96c446f
Vladimir Sedach <vas@oneofus.la>
2020-05-28 21:56:48 -0700

    check.sh: Do not load Emacs init or site files

--- check.sh
+++ check.sh
@@ -4,7 +4,7 @@ set -eu
 
 : ${EMACS:=emacs}
 
-exec ${EMACS} --batch --eval '(progn
+exec ${EMACS} -q --no-site-file --batch --eval '(progn
   (byte-compile-file "paredit.el" t)
   (byte-compile-file "test.el" t)
 )'
