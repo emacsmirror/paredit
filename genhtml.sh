b1362dba406a4425f4bffc248d5d2905dcf81563
Taylor R Campbell <campbell+paredit@mumble.net>
2020-05-29 06:09:58 +0000

    Work around new quirk in emacs --eval.
    Apparently this can't begin/end with a line break, so add a spurious
    (progn ...) around it.

--- genhtml.sh
+++ genhtml.sh
@@ -4,8 +4,8 @@ set -Ceu
 
 : ${EMACS:=emacs}
 
-exec ${EMACS} --batch --load paredit.el --eval '
+exec ${EMACS} --batch --load paredit.el --eval '(progn
 (with-temp-buffer
   (paredit-insert-html-examples)
   (write-file "paredit.html"))
-'
+)'
